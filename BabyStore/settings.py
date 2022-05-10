c058bf96370a1105805816d87dbec3072f56fe55
Nguyen An <thienan.vn93.study@gmail.com>
2022-05-10 09:04:49 +0700

    commit

--- BabyStore/settings.py
+++ BabyStore/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['babyshop-namiya.herokuapp.com', '127.0.0.1']
 
 # Lỗi CSRF verification failed. Request aborted.
-CSRF_TRUSTED_ORIGINS = ['https://babyshop-namiya.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['babyshop-namiya.herokuapp.com']
 
 # Application definition
 
